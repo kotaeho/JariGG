f4441cfc6e02a9349b06f538b404327f8be41479
kotaeho <godand11@naver.com>
2025-01-17 00:04:25 +0900

    cors 수정

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const app = express()
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    methods: 'GET,POST',
+    methods: 'GET,POST,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization'
   })
 )
