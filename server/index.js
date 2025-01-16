03303b156f5393267aa93aec6837c6a1d35977ac
kotaeho <godand11@naver.com>
2025-01-16 20:50:09 +0900

    수정

--- server/index.js
+++ server/index.js
@@ -169,10 +169,10 @@ io.on('connection', socket => {
 
 // === OAuth2 디스코드 인증 ===
 
-app.use(express.static(path.join('D:', 'React', 'dist'))) // 로컬 경로로 수정
+app.use(express.static(path.join(__dirname, 'JariGG', 'dist'))) // 'JariGG/dist' 경로로 수정
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join('D:', 'React', 'dist', 'index.html')) // 로컬 경로로 수정
+  res.sendFile(path.join(__dirname, 'JariGG', 'dist', 'index.html')) // 'JariGG/dist' 경로로 수정
 })
 
 // Step 1: OAuth2 로그인 URL 생성
