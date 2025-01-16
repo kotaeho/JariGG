ed630d9e421a135b683e9a750fc60223b821da8c
kotaeho <godand11@naver.com>
2025-01-17 00:15:54 +0900

    credentials 추가

--- server/index.js
+++ server/index.js
@@ -36,7 +36,8 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     methods: 'GET,POST,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization'
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
   })
 )
 app.use(express.json())
